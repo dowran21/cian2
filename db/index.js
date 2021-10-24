a42e57e8ab2fd33c9c564f33002511caa425d849
dowran21 <ddowran2106@gmail.com>
2021-10-24 21:49:16 +0500

    again db/index.js file

--- db/index.js
+++ db/index.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
 const Pool = require('pg').Pool;
 
-
 const pool = new Pool(
-    {
-    user: process.env.DB_USER || 'postgres',   
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'cian',
-    password: process.env.DB_PASSWORD || '61123141',
-    port: process.env.DB_PORT || '5432'
-  });
+{
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'cian',
+  password: process.env.DB_PASSWORD || '61123141',
+  port: process.env.DB_PORT || 5432
+});
  
 
 module.exports = {
