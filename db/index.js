e192f58e51599f313250b38721bcfc55e6ebb48b
dowran21 <ddowran2106@gmail.com>
2021-10-24 22:42:29 +0500

    everything go

--- db/index.js
+++ db/index.js
@@ -3,10 +3,10 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool(
 {
-  user: process.env.DB_USER || 'dowran',
+  user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'cian',
-  password: process.env.DB_PASSWORD || '61123141dow',
+  password: process.env.DB_PASSWORD || '61123141',
   port: process.env.DB_PORT || 5432
 });
  
