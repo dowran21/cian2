fa3becf4b264347788037327f9f28d50426547e0
dowran21 <ddowran2106@gamil.com>
2021-10-27 10:39:06 +0500

    working with env

--- utils/consts.js
+++ utils/consts.js
@@ -5,11 +5,7 @@ const DATABASE_PORT = process.env.DB_PORT;
 const DATABASE_HOST = process.env.DB_HOST;
 const DATABASE_USER = process.env.DB_USER;
 const DATABASE_PASSWORD = process.env.DB_PASSWORD
-console.log(DATABASE_HOST,
-    DATABASE_NAME,
-    DATABASE_PORT,
-    DATABASE_USER,
-    DATABASE_PASSWORD)
+
 module.exports = {
     DATABASE_HOST,
     DATABASE_NAME,
