35943abff2209367f4cf3c4da752394c9eea7ed7
dowran21 <ddowran2106@gamil.com>
2021-12-12 15:20:44 +0500

    adding last updates

--- index.js
+++ index.js
@@ -26,13 +26,12 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(express.static('/.well-known/pki-validation/'))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.use('/api', Routers)
 
 app.use('/uploads', express.static(dir))
 
-app.use('/.well-known/pki-validation', express.static('/.well-known/pki-validation/'))
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
