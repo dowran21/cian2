2dc0993a4b2b40d115a5dbcc6819864dade23979
dowran21 <ddowran2106@gmail.com>
2022-04-30 10:35:16 +0500

    new build with sublocations

---
+++ build/static/js/11.0142a65d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[11],{172:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.smallUp=void 0;t.smallUp={viewBox:"0 0 20 20",children:[{name:"path",attribs:{d:"M6.582,12.141c-0.271,0.268-0.709,0.268-0.978,0c-0.269-0.268-0.272-0.701,0-0.969l3.908-3.83\r\n\tc0.27-0.268,0.707-0.268,0.979,0l3.908,3.83c0.27,0.267,0.27,0.701,0,0.969c-0.271,0.268-0.709,0.268-0.979,0L10,9L6.582,12.141z"}}]}}}]);
+//# sourceMappingURL=11.0142a65d.chunk.js.map
